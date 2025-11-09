00945aa92ac616d1d847e7389ddaaa4d8226b3ac
valen0311 <vdizquierdo.24@est.ucab.edu.ve>
2025-11-09 15:49:23 -0400

    primer commit

---
+++ puppyhugs-app/src/test/java/com/puppyhugs_app/PuppyhugsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puppyhugs_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyhugsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
